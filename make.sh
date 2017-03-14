3191ca73eba380b53235b66209cb4f98786be5d1
carson.wang <carson.wang@droi.com>
2017-03-14 14:07:00 +0800

    fix

--- make.sh
+++ make.sh
@@ -14,6 +14,22 @@ THIRD_DIR='com/idroi/third'
 eval 'mkdir -p $INFOHUB_DIR/$JAR_VERSION'
 eval 'mkdir -p $THIRD_DIR/$JAR_VERSION'
 
-
 eval 'cp -r $INFOHUB_DIR/$OLD/* $INFOHUB_DIR/$JAR_VERSION'
 eval 'cp -r $THIRD_DIR/$OLD/* $THIRD_DIR/$JAR_VERSION'
+
+eval 'mv $INFOHUB_DIR/$JAR_VERSION/infohub-$OLD.pom $INFOHUB_DIR/$JAR_VERSION/infohub-$JAR_VERSION.pom'
+eval 'mv $THIRD_DIR/$JAR_VERSION/third-$OLD.pom $THIRD_DIR/$JAR_VERSION/third-$JAR_VERSION.pom'
+
+eval 'sed -ie "s/$OLD/$JAR_VERSION/g" $INFOHUB_DIR/$JAR_VERSION/infohub-$JAR_VERSION.pom'
+eval 'sed -ie "s/$OLD/$JAR_VERSION/g" $THIRD_DIR/$JAR_VERSION/third-$JAR_VERSION.pom'
+
+eval 'rm $INFOHUB_DIR/$JAR_VERSION/infohub-$JAR_VERSION.pome'
+eval 'rm $THIRD_DIR/$JAR_VERSION/third-$JAR_VERSION.pome'
+
+eval 'cp infohub-release.aar $INFOHUB_DIR/$JAR_VERSION/infohub-$JAR_VERSION.aar'
+eval 'cp ThirdAPI.jar $THIRD_DIR/$JAR_VERSION/third-$JAR_VERSION.jar'
+
+
+eval 'sed "/<version>$OLD<\/version>/a <version>$JAR_VERSION<\/version>" $INFOHUB_DIR/maven-metadata.xml'
+eval 'sed "/<version>$OLD<\/version>/a <version>$JAR_VERSION<\/version>" $THIRD_DIR/maven-metadata.xml'
+
