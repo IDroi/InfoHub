e87cb04fc1d92ab40c40f7e5bc6bc02fda8a5206
carson.wang <carson.wang@droi.com>
2017-03-17 20:50:18 +0800

    update

--- make.sh
+++ make.sh
@@ -9,38 +9,38 @@
 OLD='1.2.17'
 VERSION='1.2.18'
 INFOHUB_DIR='com/idroi/infohub'
-THIRD_DIR='com/idroi/third'
+MOPUB_DIR='com/idroi/mopub'
 
 eval 'mkdir -p $INFOHUB_DIR/$VERSION'
-eval 'mkdir -p $THIRD_DIR/$VERSION'
+eval 'mkdir -p $MOPUB_DIR/$VERSION'
 
 eval 'cp -r $INFOHUB_DIR/$OLD/* $INFOHUB_DIR/$VERSION'
-eval 'cp -r $THIRD_DIR/$OLD/* $THIRD_DIR/$VERSION'
+eval 'cp -r $MOPUB_DIR/$OLD/* $MOPUB_DIR/$VERSION'
 
 eval 'mv $INFOHUB_DIR/$VERSION/infohub-$OLD.pom $INFOHUB_DIR/$VERSION/infohub-$VERSION.pom'
-eval 'mv $THIRD_DIR/$VERSION/third-$OLD.pom $THIRD_DIR/$VERSION/third-$VERSION.pom'
+eval 'mv $MOPUB_DIR/$VERSION/mopub-$OLD.pom $MOPUB_DIR/$VERSION/mopub-$VERSION.pom'
 
 eval 'sed -i "s/$OLD/$VERSION/g" $INFOHUB_DIR/$VERSION/infohub-$VERSION.pom'
-eval 'sed -i "s/$OLD/$VERSION/g" $THIRD_DIR/$VERSION/third-$VERSION.pom'
+eval 'sed -i "s/$OLD/$VERSION/g" $MOPUB_DIR/$VERSION/mopub-$VERSION.pom'
 
 eval 'rm $INFOHUB_DIR/$VERSION/infohub-$OLD.aar'
-eval 'rm $THIRD_DIR/$VERSION/third-$OLD.jar'
+eval 'rm $MOPUB_DIR/$VERSION/mopub-$OLD.jar'
 #eval 'rm $INFOHUB_DIR/$VERSION/infohub-$VERSION.pome'
-#eval 'rm $THIRD_DIR/$VERSION/third-$VERSION.pome'
+#eval 'rm $MOPUB_DIR/$VERSION/mopub-$VERSION.pome'
 
 eval 'cp infohub-release.aar $INFOHUB_DIR/$VERSION/infohub-$VERSION.aar'
-eval 'cp ThirdAPI.jar $THIRD_DIR/$VERSION/third-$VERSION.jar'
+eval 'cp mopub-sdk-4.11.0.jar $MOPUB_DIR/$VERSION/mopub-$VERSION.jar'
 
 
 INFOHUB_META='com/idroi/infohub/maven-metadata.xml'
-THIRD_META='com/idroi/third/maven-metadata.xml'
+MOPUB_META='com/idroi/mopub/maven-metadata.xml'
 
 eval 'sed -i "/<release>/c\ \ \ \ <release>$VERSION<\/release>" $INFOHUB_META'
-eval 'sed -i "/<release>/c\ \ \ \ <release>$VERSION<\/release>" $THIRD_META'
+eval 'sed -i "/<release>/c\ \ \ \ <release>$VERSION<\/release>" $MOPUB_META'
 
 eval 'sed -i "/<version>$OLD<\/version>/a \ \ \ \ \ \ <version>$VERSION<\/version>" $INFOHUB_META'
-eval 'sed -i "/<version>$OLD<\/version>/a \ \ \ \ \ \ <version>$VERSION<\/version>" $THIRD_META'
+eval 'sed -i "/<version>$OLD<\/version>/a \ \ \ \ \ \ <version>$VERSION<\/version>" $MOPUB_META'
 
 NOW=$(date +'%Y%m%d%H%M%S')
 eval 'sed -i "/<lastUpdated>/c\ \ \ \ <lastUpdated>$NOW<\/lastUpdated>" $INFOHUB_META'
-eval 'sed -i "/<lastUpdated>/c\ \ \ \ <lastUpdated>$NOW<\/lastUpdated>" $THIRD_META'
+eval 'sed -i "/<lastUpdated>/c\ \ \ \ <lastUpdated>$NOW<\/lastUpdated>" $MOPUB_META'
