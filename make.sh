c7efaf40d538475c003f64b69d5755191010e5c9
carson.wang <carson.wang@droi.com>
2017-03-25 13:48:50 +0800

    update make

--- make.sh
+++ make.sh
@@ -8,28 +8,33 @@
 
 project_name='infohub'
 
-old='1.2.21'
-version='1.2.22'
+old='1.2.22'
+new='1.2.23'
 dir='com/idroi/infohub'
 
+old_dir="$dir/$old"
+new_dir="$dir/$new"
+
 #-- create directory for new version
-eval 'mkdir -p $dir/$version'
-eval 'cp -r $dir/$old/* $dir/$version'
-eval 'mv $dir/$version/$project_name-$old.pom $dir/$version/$project_name-$version.pom'
+mkdir -p $new_dir
+cp -r $old_dir/* $new_dir
+mv $new_dir/$project_name-$old.pom $new_dir/$project_name-$new.pom
 
 
 #-- revise aar and pom
-eval 'sed -i "s/$old/$version/g" $dir/$version/$project_name-$version.pom'
-eval 'rm $dir/$version/$project_name-$old.aar'
-eval 'cp $project_name-release.aar $dir/$version/$project_name-$version.aar'
+sed -i "s/$old/$new/g" $new_dir/$project_name-$new.pom
+rm $new_dir/$project_name-$old.aar
+cp $project_name-release.aar $new_dir/$project_name-$new.aar
 
 
 #-- change directory to revise maven-metadata and last updated
 cd $dir
-eval 'sed -i "/<release>/c\ \ \ \ <release>$version<\/release>" maven-metadata.xml'
-eval 'sed -i "/<version>$old<\/version>/a \ \ \ \ \ \ <version>$version<\/version>" maven-metadata.xml'
+meta='maven-metadata.xml'
 
-NOW=$(date +'%Y%m%d%H%M%S')
-eval 'sed -i "/<lastUpdated>/c\ \ \ \ <lastUpdated>$NOW<\/lastUpdated>" maven-metadata.xml'
+sed -i "/<release>/c\ \ \ \ <release>$new<\/release>" $meta
+sed -i "/<version>$old<\/version>/a \ \ \ \ \ \ <version>$new<\/version>" $meta
+
+now=$(date +'%Y%m%d%H%M%S')
+sed -i "/<lastUpdated>/c\ \ \ \ <lastUpdated>$now<\/lastUpdated>" $meta
 
 cd -
